d6ebdb3f31214ab3aeb53fd808ad8f40a779aa90
Bianca <bboldeman@gmail.com>
2025-10-29 12:13:11 +1100

    Setup commit

---
+++ example.py
@@ -0,0 +1,98 @@
+#!/usr/bin/env pybricks-micropython
+from pybricks.hubs import EV3Brick
+from pybricks.ev3devices import (Motor, TouchSensor, ColorSensor,
+                                 InfraredSensor, UltrasonicSensor, GyroSensor)
+from pybricks.parameters import Port, Stop, Direction, Button, Color
+from pybricks.tools import wait, StopWatch, DataLog
+from pybricks.robotics import DriveBase
+from pybricks.media.ev3dev import SoundFile, ImageFile
+
+
+class BagelBot(EV3Brick):
+    def __init__(self, 
+    left_motor_port, 
+    right_motor_port,
+    attachment_motor_port_1=None,
+    attachment_motor_port_2=None,
+    color_sensor_port=None,
+    touch_sensor_port=None,
+    ultrasonic_sensor_port=None,
+    gyro_sensor_port=None,
+    ):
+        self.left_motor = Motor(left_motor_port)
+        self.right_motor = Motor(right_motor_port)
+        self.drive_base = DriveBase(self.left_motor, self.right_motor, wheel_diameter=56, axle_track=114)
+        self.reset()
+
+        self.attachment_motor_port_1 = attachment_motor_port_1
+        self.attachment_motor_port_2 = attachment_motor_port_2
+
+        if attachment_motor_port_1:
+            self.attachment_motor_1 = Motor(attachment_motor_port_1)
+        if attachment_motor_port_2:
+            self.attachment_motor_2 = Motor(attachment_motor_port_2)
+        if color_sensor_port:
+            self.color_sensor = ColorSensor(color_sensor_port)
+        if touch_sensor_port:
+            self.touch_sensor = TouchSensor(touch_sensor_port)
+        if ultrasonic_sensor_port:
+            self.ultrasonic_sensor = UltrasonicSensor(ultrasonic_sensor_port)
+        if gyro_sensor_port:
+            self.gyro_sensor = GyroSensor(gyro_sensor_port)
+            self.gyro_sensor.reset_angle()
+
+    def configureSettings(self, speed, acceleration, turnRate, turnAcceleration):
+        self.settings(speed, acceleration, turnRate, turnAcceleration)
+
+    def move(self, distance):
+        self.drive_base.straight(distance)
+
+    def turn(self, angle):
+        if self.gyro_sensor:
+            currentAngle = self.gyro_sensor.angle()
+        else:
+            currentAngle = self.angle()
+        if angle > currentAngle:
+            targetAngle = currentAngle + angle
+        else:
+            targetAngle = currentAngle - angle
+        
+        self.drive_base.turn(targetAngle)
+
+    def moveAttachment(self, motor_port, speed, angle, stopType=Stop.COAST, wait=False):
+        if motor_port == self.attachment_motor_port_1:
+            self.attachment_motor_1.run_angle(speed, angle, stopType, wait)
+        elif motor_port == self.attachment_motor_port_2:
+            self.attachment_motor_2.run_angle(speed, angle, stopType, wait)
+        else:
+            raise ValueError("Invalid attachment motor port")
+
+    def pressed(self):
+        return self.touch_sensor.pressed()
+    
+    def distance(self):
+        return self.ultrasonic_sensor.distance()
+    
+    def color(self):
+        return self.color_sensor.color()
+    
+    def angle(self):
+        return self.gyro_sensor.angle()
+    
+        
+### Your code goes here ###
+
+# Initialise the robot with motors and sensors
+robot = BagelBot(
+    left_motor_port=Port.A,
+    right_motor_port=Port.B,
+    attachment_motor_port_1=Port.C,
+    touch_sensor_port=Port.S1)
+
+while True: # Loop forever
+    if robot.pressed(): # If the touch sensor is pressed
+        robot.move(-200) # Move backward 200 mm
+        wait(1000) # Wait for 1 second
+        robot.turn(180) # Turn around
+    robot.move(500) # Move forward 500 mm
+    robot.moveAttachment(motor_port=Port.C, speed=500, angle=90) # Move attachment motor
---
+++ readme.md
@@ -0,0 +1,8 @@
+# Battlebots season is upon us!
+Welcome to Github! In this repository, you will find the code you need to get started!
+
+## BattleMods
+This is the library that you will be using to help program the robot! Feel free to read through what it does, but simply it just condenses the objects and functions that you call, as well as adding a few quality of life additions.
+
+## example.py
+Not sure where to start? This program shows you how to initialise your robot. It also goes over the basics of loops, if statements and movement. 
