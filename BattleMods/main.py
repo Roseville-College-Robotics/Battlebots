037967d26b7010f310c0d1574d2a63b98e2c97ab
HappyShoelace <bboldeman@gmail.com>
2025-11-05 16:13:36 +1100

    Matched class to example file

--- BattleMods/main.py
+++ BattleMods/main.py
@@ -8,7 +8,7 @@ from pybricks.robotics import DriveBase
 from pybricks.media.ev3dev import SoundFile, ImageFile
 
 
-class BagelBot(EV3Brick):
+class Bagel_bot(EV3Brick):
     def __init__(self, 
     left_motor_port, 
     right_motor_port,
@@ -19,65 +19,86 @@ class BagelBot(EV3Brick):
     ultrasonic_sensor_port=None,
     gyro_sensor_port=None,
     ):
+        super().__init__()
         self.left_motor = Motor(left_motor_port)
+        
         self.right_motor = Motor(right_motor_port)
-        self.drive_base = DriveBase(self.left_motor, self.right_motor, wheel_diameter=56, axle_track=114)
-        self.reset()
+        
+        # Create DriveBase - wheel_diameter and axle_track are in millimeters
+        # Try positional args in order: (left_motor, right_motor, wheel_diameter, axle_track)
+        try:
+            print("Attempting to create DriveBase with positional args...")
+            self.drive_base = DriveBase(self.left_motor, self.right_motor, 56, 114)
+        except Exception as e:
+            # Re-raise with more context so the EV3 log shows helpful details
+            print("Failed to create DriveBase:", e)
+            print("Left motor details:", self.left_motor)
+            print("Right motor details:", self.right_motor)
+            raise
+        # Some versions of DriveBase may not implement reset(); avoid calling it here
+        print("Drive base initialized.")
 
         self.attachment_motor_port_1 = attachment_motor_port_1
         self.attachment_motor_port_2 = attachment_motor_port_2
 
         if attachment_motor_port_1:
+            print("Attachment motor 1 initialized.")
             self.attachment_motor_1 = Motor(attachment_motor_port_1)
         if attachment_motor_port_2:
+            print("Attachment motor 2 initialized.")
             self.attachment_motor_2 = Motor(attachment_motor_port_2)
         if color_sensor_port:
+            print("Color sensor initialized.")
             self.color_sensor = ColorSensor(color_sensor_port)
         if touch_sensor_port:
+            print("Touch sensor initialized.")
             self.touch_sensor = TouchSensor(touch_sensor_port)
         if ultrasonic_sensor_port:
+            print("Ultrasonic sensor initialized.")
             self.ultrasonic_sensor = UltrasonicSensor(ultrasonic_sensor_port)
         if gyro_sensor_port:
+            print("Gyro sensor initialized.")
             self.gyro_sensor = GyroSensor(gyro_sensor_port)
             self.gyro_sensor.reset_angle()
 
-    def configureSettings(self, speed, acceleration, turnRate, turnAcceleration):
-        self.settings(speed, acceleration, turnRate, turnAcceleration)
+    def configure_settings(self, speed, acceleration, turnRate, turnAcceleration):
+        self.drive_base.settings(speed, acceleration, turnRate, turnAcceleration)
 
     def move(self, distance):
         self.drive_base.straight(distance)
 
     def turn(self, angle):
-        if self.gyro_sensor:
-            currentAngle = self.gyro_sensor.angle()
-        else:
-            currentAngle = self.angle()
-        if angle > currentAngle:
-            targetAngle = currentAngle + angle
-        else:
-            targetAngle = currentAngle - angle
-        
-        self.drive_base.turn(targetAngle)
+        self.drive_base.turn(angle)
 
-    def moveAttachment(self, motor_port, speed, angle, stopType=Stop.COAST, wait=False):
+    def move_attachment(self, motor_port, speed, angle, stopType=Stop.COAST, wait=False):
         if motor_port == self.attachment_motor_port_1:
-            self.attachment_motor_1.run_angle(speed, angle, stopType, wait)
+            self.attachment_motor_1.run_angle(speed, angle, then=stopType, wait=wait)
         elif motor_port == self.attachment_motor_port_2:
-            self.attachment_motor_2.run_angle(speed, angle, stopType, wait)
+            self.attachment_motor_2.run_angle(speed, angle, then=stopType, wait=wait)
         else:
             raise ValueError("Invalid attachment motor port")
 
     def pressed(self):
-        return self.touch_sensor.pressed()
-    
+        if hasattr(self, 'touch_sensor'):
+            return self.touch_sensor.pressed()
+        return None
+
     def distance(self):
-        return self.ultrasonic_sensor.distance()
-    
+        if hasattr(self, 'ultrasonic_sensor'):
+            return self.ultrasonic_sensor.distance()
+        return None
+
     def color(self):
-        return self.color_sensor.color()
-    
+        if hasattr(self, 'color_sensor'):
+            return self.color_sensor.color()
+        return None
+
     def angle(self):
-        return self.gyro_sensor.angle()
-        
+        if hasattr(self, 'gyro_sensor'):
+            return self.gyro_sensor.angle()
+        return None
+
+    def beep(self):
+        self.speaker.beep()
 
 ### Your code goes here ###
